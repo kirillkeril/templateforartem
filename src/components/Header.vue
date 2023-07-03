827de29c304c17fc8751872686a2024ecfa1b7ed
Your Name <you@example.com>
2023-07-03 23:50:38 +0500

    add header components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,18 @@
 <template>
-  <header>
-    <button>"аапвпва"</button>
-    <button>аывапв</button>
+  <header class="header">
+    <div class="logo">
+        <h1>рецепт.ру</h1>
+    </div>
+    <div class="search">
+      <div class="searchInput">
+        <input type ="text" class="search1">
+        <div class="icon"></div>
+      </div>
+    </div>
+    <div class="account">
+      <button class="loginButton">Войти</button>
+      <button class="profile"></button>
+    </div>
   </header>
 </template>
 
@@ -15,6 +26,56 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.header{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows:50fr auto ;
+  grid-template-areas:"logo account"
+    "search search";
+  align-items: center;
+  margin: 40px;
 
+}
+.logo{
+  grid-area: logo;
+}
+.account{
+  grid-area: account;
+  display:flex;
+  justify-content: flex-end;
+}
+.search{
+  grid-area: search;
+  position: relative;
+}
+.icon{
+  background: black;
+  width: 10px;
+  height: 10px;
+  position: absolute;
+  top:50%;
+  right: 1px;
+  transform: translateY(-50%);
+}
+.search1{
+width: 100%;
+  border-radius: 10px;
+}
+.loginButton{
+  background: none;
+  border: none;
+  font-family: "Rubik Mono One",sans-serif;
+}
+.logo {
+  font-family:  "Raleway",sans-serif;;
+
+}
+.profile{
+  border-radius: 15px;
+  width: 40px;
+  height: 40px;
+  border:2px solid black ;
+
+}
 </style>
