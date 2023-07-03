549296559efd05b39e3aa410af54e974407938cd
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-07-03 22:43:17 +0500

    fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,7 @@
 import AppLayout from "./AppLayout.vue";
-import BlankLayout from "./BlankLayout.vue";
 import DefaultLayout from "./DefaultLayout.vue";
 
 export default {
     AppLayout,
-    BlankLayout,
     DefaultLayout
 }
