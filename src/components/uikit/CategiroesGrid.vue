f721e1955ddcae102fb237c43d39b30ab51cf24c
Your Name <you@example.com>
2023-07-04 22:08:42 +0500

    добавил сетку продуктов

---
+++ src/components/uikit/CategiroesGrid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="categories">
+    <input type="button" class="ff" >
+    <input type="button" class="ff" >
+    <input type="button" class="ff" >
+
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style  scoped>
+.categories {
+  width: 100vw;
+  grid-template-columns: 1fr;
+  display: grid;
+  justify-items: center;
+}
+.ff{
+  border: none;
+  border-radius: 40px;
+  margin: 30px 20px;
+  height: 225px;
+  width: 326px;
+  background: aqua;
+}
+
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import NavMenu from "@/components/NavMenu.vue";
+import CategiroesGrid from "@/components/uikit/CategiroesGrid.vue";
 </script>
 
 <template>
   <div class="layout">
     <Header></Header>
     <NavMenu/>
+    <categiroes-grid></categiroes-grid>
     <slot/>
   </div>
 </template>
